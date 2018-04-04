44d52180a9daef3f2e5bc31fe090c8d0d8233625
Basil R. Yap <basil.y.j.t@gmail.com>
2018-04-04 19:52:40 +0800

    Changed index to allow for parallel query

--- img_extract/core.py
+++ img_extract/core.py
@@ -1,28 +1,32 @@
 import helper
 import pandas as pd
 
-def main(file, dir):
+def main(start, file, dir):
 	dataset = pd.read_csv("./data/%s" % file, dtype="str")
 	print("########## START SCRIPT ##########")
-	table = {}
+	# table = {}
 	n = dataset.shape[0]
 	size = 0
-	for i in range(20166,n):
-		ldmkId = dataset["landmark_id"][i]
-		if ldmkId in table.keys():
-			table[ldmkId] += 1
-			imgId = ldmkId +"-2-"+ str(table[ldmkId])
-		else:
-			table[ldmkId] = 1
-			imgId = ldmkId + "-2-1"
+	for i in range(start,n):
+		# ldmkId = dataset["landmark_id"][i]
+		
+		imgId = dataset["landmark_id"][i] + "-" + dataset["id"][i]
+
+		# if ldmkId in table.keys():
+		# 	table[ldmkId] += 1
+		# 	imgId = ldmkId + "-6-" + str(table[ldmkId])
+		# else:
+		# 	table[ldmkId] = 1
+		# 	imgId = ldmkId + "-6-1"
 
 		size += helper.getImage(imgId, dataset["url"][i], dir)
-		print(helper.getProgress(i, n, size)+" "+imgId)
+		print("%s %s : %s" %(helper.getProgress(i, n, size), i, imgId))
 	print(">>> Log BEGIN")
-	with open("./logs/total_count.csv", "a") as output:	
-		for key in table:
-			output.write("%s, %s" %(key, table[key]))
+	# with open("./logs/total_count.csv", "a") as output:	
+	# 	for key in table:
+	# 		output.write("%s, %s" %(key, table[key]))
 	print(">>> Log END")
 	print("########## END SCRIPT ##########")
 
-main("recognition/train.csv", "/run/media/dekatria/My Passport/kaggle_dataset/google_recognition/train/")
+# main("recognition/train.csv", "/run/media/dekatria/My Passport/kaggle_dataset/google_recognition/train/")
+main(338324,"recognition/train.csv", "/home/dekatria/Downloads/kaggle_dataset/google_recognition/train/")
