058b73898e7799df5246fae7b3c67a2a03975b62
Basil R. Yap <basil.y.j.t@gmail.com>
2018-04-06 05:46:57 +0800

    Cleaned up for use in command line

--- img_extract/core.py
+++ img_extract/core.py
@@ -1,32 +1,28 @@
+"""
+PURPOSE: extract image from the links given in the dataset.
+"""
+
 import helper
 import pandas as pd
+import sys
 
-def main(start, file, dir):
+def main(file, dir, start=0):
 	dataset = pd.read_csv("./data/%s" % file, dtype="str")
 	print("########## START SCRIPT ##########")
-	# table = {}
 	n = dataset.shape[0]
 	size = 0
 	for i in range(start,n):
-		# ldmkId = dataset["landmark_id"][i]
-		
 		imgId = dataset["landmark_id"][i] + "-" + dataset["id"][i]
-
-		# if ldmkId in table.keys():
-		# 	table[ldmkId] += 1
-		# 	imgId = ldmkId + "-6-" + str(table[ldmkId])
-		# else:
-		# 	table[ldmkId] = 1
-		# 	imgId = ldmkId + "-6-1"
-
 		size += helper.getImage(imgId, dataset["url"][i], dir)
 		print("%s %s : %s" %(helper.getProgress(i, n, size), i, imgId))
 	print(">>> Log BEGIN")
-	# with open("./logs/total_count.csv", "a") as output:	
-	# 	for key in table:
-	# 		output.write("%s, %s" %(key, table[key]))
 	print(">>> Log END")
 	print("########## END SCRIPT ##########")
 
-# main("recognition/train.csv", "/run/media/dekatria/My Passport/kaggle_dataset/google_recognition/train/")
-main(338324,"recognition/train.csv", "/home/dekatria/Downloads/kaggle_dataset/google_recognition/train/")
+if __name__ == "__main__":
+	if len(sys.argv) == 2:
+		main(sys.argv[0], sys.argv[1])
+	elif len(sys.argv) == 3:
+		main(sys.argv[0], sys.argv[1], sys.argv[2])
+	else:
+		raise IndexError("Invalid number of parameters")
